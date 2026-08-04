1af002d9a3235cad8909111abc5c89a0ea8df802
Evgeniy Kvasov <eakvasov@yandex.ru>
2026-08-04 20:30:28 +0300

    First commit: C programs

---
+++ arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	
+	int my_array[4] = {1, 2, 3, 4};
+
+	for(int i = 0; i < 4; i++){
+
+		my_array[i] = my_array[i] * 2;
+
+		printf(" %d ", my_array[i]);
+	}
+
+	return 0;
+}
